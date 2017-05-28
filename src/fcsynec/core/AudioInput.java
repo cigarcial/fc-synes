a028e636d57b398aa0dfb7a04d52282ba3ce0b25
Felipe Villarreal Daza <fvillarreald@unal.edu.co>
2017-05-28 17:20:42 -0500

    Agrego nueva clase a petición de Ciro.

---
+++ src/fcsynec/core/AudioInput.java
@@ -0,0 +1,36 @@
+package fcsynec.core;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.*;
+import com.dazz.audio.*;
+
+public class AudioInput {
+	
+	private ConcurrentLinkedDeque<Integer> data;
+	private Thread thread;
+	
+	public AudioInput() {
+		data = new ConcurrentLinkedDeque<>();
+		si = new SignalInput();
+		si.initInput();
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public int next() {
+		return (data.peek() == null)? 0 : data.poll();
+	}
+	
+	@Override
+	public void run() {
+		while(true){
+			int tmp = si.getEvent();
+			if (tmp != -1)
+				data.offer(tmp);
+			try{
+				Thread.sleep(200);
+			}catch(Exception ex){}
+		}
+	}
+}
